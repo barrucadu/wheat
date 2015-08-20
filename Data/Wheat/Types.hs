453e90abc92fc216f6f4ee9dae74a244cfe5d0f4
Michael Walker <mike@barrucadu.co.uk>
2015-08-20 12:12:13 +0100

    Add some more instances

--- Data/Wheat/Types.hs
+++ Data/Wheat/Types.hs
@@ -10,9 +10,12 @@
 -- types along with a function to split the whole.
 module Data.Wheat.Types where
 
+import Control.Applicative
 import Control.Arrow
+import Control.Monad
 import Data.Functor.Contravariant
 import Data.Functor.Contravariant.Divisible
+import Data.Maybe
 import Data.Monoid
 import Data.Void
 
@@ -47,6 +50,11 @@ instance Applicative Decoder where
     Just (f, b') -> first f <$> runDecoder da b'
     Nothing -> Nothing
 
+instance Alternative Decoder where
+  empty = Decoder $ const Nothing
+
+  da <|> db = Decoder $ \b -> fromMaybe (runDecoder db b) (Just <$> runDecoder da b)
+
 instance Monad Decoder where
   return = pure
 
@@ -54,9 +62,15 @@ instance Monad Decoder where
     Just (a, b') -> runDecoder (f a) b'
     Nothing      -> Nothing
 
+instance MonadPlus Decoder
+
 -- | Run a codec's decoder.
 decode :: L.ByteString -> Codec' d e -> Maybe d
-decode b (d, _) = fst <$> runDecoder d b
+decode b c = fst <$> runDecoder (decoderOf c) b
+
+-- | Get the decoder of a codec.
+decoderOf :: Codec' d e -> Decoder d
+decoderOf = fst
 
 -- * Encoders
 
@@ -80,6 +94,14 @@ instance Decidable Encoder where
 
   lose f = Encoder $ absurd . f
 
+instance Monoid (Encoder a) where
+  mempty = Encoder $ const mempty
+  mappend e1 e2 = Encoder $ \a -> runEncoder e1 a <> runEncoder e2 a
+
 -- | Run a codec's encoder.
 encode :: e -> Codec' d e -> B.Builder
-encode a (_, e) = runEncoder e a
+encode a c = runEncoder (encoderOf c) a
+
+-- | Get the encoder of a codec.
+encoderOf :: Codec' d e -> Encoder e
+encoderOf = snd
